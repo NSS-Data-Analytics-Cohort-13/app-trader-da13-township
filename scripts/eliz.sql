6e4bcf3cd073da360a712bd35dc1608589266b8f
ebestford <elizgracbest@gmail.com>
2024-10-19 19:11:54 -0400

    finished query

--- scripts/eliz.sql
+++ scripts/eliz.sql
@@ -6,7 +6,38 @@ SELECT *
 FROM play_store_apps
 ORDER BY name;
 
+---Formula (self join) that Dibran shared. Here we are seeing the different copies of each app in the play store first, then the apple store. The next move will be to use the MAX and maybe the AVG of the apps to find the most recent and to find the average of the ratings. 
+select * 
+from play_store_apps
+where name in (
+
+select distinct a.name
+from play_store_apps a
+inner join play_store_apps b
+	on a.name = b.name
+where a.review_count != b.review_count
+
+)
+order by name, review_count desc
+
+---Formula (self join) for apple store
+select * 
+from app_store_apps
+where name in (
+
+select distinct a.name
+from app_store_apps a
+inner join app_store_apps b
+	on a.name = b.name
+where a.review_count != b.review_count
+
+)
+order by name, review_count desc
+
+
+
 --Using Union function to pull apps that are in both columns. Using case statement to structure store column to output apple store or play store in relation to rows pulled. 
+
 SELECT name,
 	   price,
 	   review_count,
@@ -34,54 +65,46 @@ SELECT  name,
 FROM play_store_apps
 ORDER BY name;
 
----Dibran formula, double checking to see if there are copies (there are) and seeing how drastically different the apps are. recommended to then do avg for rating and review count and MAX function to find the most recent app created
-select * 
-from play_store_apps
-where name in (
 
-select distinct a.name
-from play_store_apps a
-inner join play_store_apps b
-	on a.name = b.name
-where a.review_count != b.review_count
-
-)
-order by name, review_count desc
-
-
----Using MAX, AVG, and ROUND functions to pull the most recent and combine the avgs of apps rating/review count to give good idea about which apps are most popular. Used AS to create aliases. Used inner join and removed CASE because not needed here. Joined on name between both tables, made it so the price had to be 0, and review count had to be above a certain amount to see hightest reviews, also made it so rating was higher than 4. The union and inner join are redundant but review error message by select when its run without it. 
+---Using DISTINCT to find only 1 copy of each app in both stores, MAX, AVG, and ROUND functions to pull the most recent and combine the avgs of apps rating/review count to give good idea about which apps are most popular. Used AS to create aliases. Used inner join. Joined on name between both tables, made it so the price had to be 0, and review count had to be above a certain amount to see hightest reviews, also made it so rating was higher than 4. The union and inner join are redundant but review error message by select when its run without it. 
 
 SELECT 
-	app_store_apps.name AS name,
-	(MAX(app_store_apps.price)::MONEY) AS price,
-	ROUND(AVG(app_store_apps.review_count)) AS review_count,
-	ROUND(AVG(app_store_apps.rating)) AS rating,
-	app_store_apps.price + '10000' AS purchase_price,
-	app_store_apps.price + '9000' AS monthly_profit,
-	app_store_apps.price + '98000' AS yearly_profit,
-	(ROUND(app_store_apps.rating)) * 2.25 AS lifespan
+	DISTINCT app_store_apps.name AS name, --finding single copy of app of name in app store
+	CAST(MAX(app_store_apps.price) AS MONEY) AS price, ---casting integer price as money
+	ROUND(MAX(app_store_apps.review_count)) AS review_count,
+	 MAX(ROUND(app_store_apps.rating *2.0)/ 2.0) AS rating,     -----round funnction for the rating to round to the highest .5
+	CASE	
+		WHEN app_store_apps.price = CAST(0 AS MONEY) THEN CAST (1000 AS MONEY)     ---better way to change integer to money but this also works
+		END AS app_cost,
+	CAST((ROUND(AVG(play_store_apps.rating)*2.0)/2.0)*2 +1 AS DECIMAL (5,2)) AS lifespan ----round rating again--dean did this formula still a little confused on it 
 FROM app_store_apps
 INNER JOIN play_store_apps
 ON app_store_apps.name=play_store_apps.name
-WHERE app_store_apps.price = '0' AND app_store_apps.rating  >=4 AND app_store_apps.review_count > 1000000
+WHERE app_store_apps.price = CAST(0 AS MONEY) AND app_store_apps.rating  >=4 AND app_store_apps.review_count > 1000000 ---rating equal to or higher than 4, review count higher than 1 million
 GROUP BY app_store_apps.name, app_store_apps.price, app_store_apps.rating
+----repeat all for play store
 UNION
+
 SELECT
-	play_store_apps.name AS name,
-	(MAX(play_store_apps.price)::MONEY) AS price,
-	ROUND(AVG(play_store_apps.review_count)) AS review_count,
-	ROUND(AVG(play_store_apps.rating)) AS rating,
-	play_store_apps.price + '10000' AS purchase_price,
-	play_store_apps.price + '9000' AS monthly_profit,
-	play_store_apps.price + '98000' AS yearly_profit,
-	(ROUND(play_store_apps.rating)) * 2.25 AS lifespan
+	DISTINCT play_store_apps.name AS name,
+	CAST(MAX(play_store_apps.price) AS MONEY) AS price,
+	ROUND(MAX(play_store_apps.review_count)) AS review_count,
+	MAX(ROUND(play_store_apps.rating *2.0)/ 2.0) AS rating,
+	CASE	
+		WHEN play_store_apps.price = CAST(0 AS MONEY) THEN CAST (1000 AS MONEY)
+		END AS app_cost,
+	CAST((ROUND(AVG(play_store_apps.rating)*2.0)/2.0)*2 +1 AS DECIMAL (5,2)) AS lifespan
 FROM play_store_apps
 INNER JOIN app_store_apps
 ON play_store_apps.name=app_store_apps.name
-WHERE play_store_apps.price = '0' AND play_store_apps.rating  >= 4 AND play_store_apps.review_count > 1000000
+WHERE play_store_apps.price = CAST(0 AS MONEY) AND play_store_apps.rating  >= 4 AND play_store_apps.review_count > 1000000
+
 GROUP BY play_store_apps.name, play_store_apps.price,play_store_apps.rating
-ORDER BY review_count DESC
-LIMIT 10;
+ORDER BY review_count DESC ----desc for highest review
+LIMIT 10; --top ten
+
+
+
 
 
 
